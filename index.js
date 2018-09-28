465e26110a7366d6bf6bc99cd84ae431bb6498ed
tcdw <tcdw2011@gmail.com>
2018-09-28 21:43:30 +0800

    大幅完善

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const fs = require('fs');
 const path = require('path');
 const ejs = require('ejs');
 const util = require('util');
+const fileSize = require('file-size');
 
 const ejsRenderFile = util.promisify(ejs.renderFile);
 const templateFile = path.resolve(__dirname, 'template/index.ejs');
@@ -16,10 +17,11 @@ if (argv.h || argv.help) {
 const host = typeof argv.h === 'string' ? argv.h : '127.0.0.1';
 const port = typeof argv.p === 'number' ? argv.p : 8888;
 const siteTitle = typeof argv.t === 'string' ? argv.t : '文件服务器';
-const dir = typeof argv[0] !== 'undefined' ? path.resolve(process.cwd(), argv[0]) : process.cwd();
+const dir = typeof argv._[0] !== 'undefined' ? path.resolve(process.cwd(), argv._[0]) : process.cwd();
 
 http.createServer(async (request, response) => {
-    const userPath = request.url.slice(1);
+    const url = decodeURIComponent(request.url);
+    const userPath = url.slice(1);
     const computedPath = path.resolve(dir, userPath);
     try {
         if (!fs.lstatSync(computedPath).isDirectory()) {
@@ -40,22 +42,31 @@ http.createServer(async (request, response) => {
             const tempInfo = fs.statSync(path.join(dir, userPath, list[i]));
             fileList[i] = {
                 name: list[i],
-                size: tempInfo.size,
-                url: `${path.join(request.url, list[i])}${tempInfo.isDirectory() ? '/' : ''}`,
+                size: fileSize(tempInfo.size).human(),
+                url: `${path.join(url, list[i])}${tempInfo.isDirectory() ? '/' : ''}`,
             };
         } catch (e) {
             console.error(e.toString());
         }
     }
-    const pathInfo = request.url.split('/');
+    const breadCrumb = [{
+        name: 'Root',
+        url: '/',
+    }];
+    const pathInfo = url.split('/');
     pathInfo.shift();
     if (pathInfo[pathInfo.length - 1] === '') {
         pathInfo.pop();
     }
+    for (let i = 0; i < pathInfo.length; i += 1) {
+        breadCrumb[i + 1] = {
+            name: pathInfo[i],
+        };
+    }
     response.writeHead(200, { 'Content-Type': 'text/html' });
     response.end(await ejsRenderFile(templateFile, {
         siteTitle,
-        breadCrumb: [],
+        breadCrumb,
         fileList,
     }, {}).catch(() => {
         response.end('Internal server error');
