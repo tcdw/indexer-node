bf7b8e43cd472722b79327f1518da36eeff176aa
tcdw <tcdw2011@gmail.com>
2018-09-29 00:43:31 +0800

    调整

--- index.js
+++ index.js
@@ -6,15 +6,14 @@ const fs = require('fs');
 const path = require('path');
 const ejs = require('ejs');
 const util = require('util');
-const fileSize = require('file-size');
 
 const ejsRenderFile = util.promisify(ejs.renderFile);
 const templateFile = path.resolve(__dirname, 'template/index.ejs');
 
 if (argv.h || argv.help) {
-    console.log('usage: indexer [-h host] [-p port_number] [-t website_title] [directory]');
+    console.log('usage: indexer [-b bind] [-p port_number] [-t website_title] [directory]');
 }
-const host = typeof argv.h === 'string' ? argv.h : '127.0.0.1';
+const bind = typeof argv.b === 'string' ? argv.b : '127.0.0.1';
 const port = typeof argv.p === 'number' ? argv.p : 8888;
 const siteTitle = typeof argv.t === 'string' ? argv.t : '文件服务器';
 const dir = typeof argv._[0] !== 'undefined' ? path.resolve(process.cwd(), argv._[0]) : process.cwd();
@@ -40,11 +39,10 @@ http.createServer(async (request, response) => {
     for (let i = 0; i < list.length; i += 1) {
         try {
             const tempInfo = fs.statSync(path.join(dir, userPath, list[i]));
-            fileList[i] = {
+            fileList.push({
                 name: list[i],
-                size: fileSize(tempInfo.size).human(),
                 url: `${path.join(url, list[i])}${tempInfo.isDirectory() ? '/' : ''}`,
-            };
+            });
         } catch (e) {
             console.error(e.toString());
         }
@@ -59,9 +57,9 @@ http.createServer(async (request, response) => {
         pathInfo.pop();
     }
     for (let i = 0; i < pathInfo.length; i += 1) {
-        breadCrumb[i + 1] = {
+        breadCrumb.push({
             name: pathInfo[i],
-        };
+        });
     }
     response.writeHead(200, { 'Content-Type': 'text/html' });
     response.end(await ejsRenderFile(templateFile, {
@@ -72,6 +70,6 @@ http.createServer(async (request, response) => {
         response.end('Internal server error');
     }));
     return true;
-}).listen(port);
+}).listen(bind);
 
-console.log(`Server running at http://${host}:${port}/`);
+console.log(`Server running at http://${bind}:${port}/`);
